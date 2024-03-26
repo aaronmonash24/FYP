1b1ff4fcabd97e40fe1925c4aa6620d9a71a293f
Chloeangggg <96276634+Chloeangggg@users.noreply.github.com>
2024-03-27 10:30:39 +1100

    Add files via upload

---
+++ Streamlit/streamlit-testing.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import mysql.connector
+import pandas as pd
+
+
+
+connection = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'Keer2001!',
+    database = "new_schema"
+)
+
+
+st.title('my FIT3164 streamlit hehe')
+
+print('connected')
+
+# connect to mysql
+cursor = connection.cursor()
+cursor.execute("Select * from food2")
+data = cursor.fetchall()
+print(cursor.column_names)
+
+df = pd.DataFrame(data,columns = cursor.column_names)
+
+# search bar
+text_search = st.text_input("Search by ID", value="")
+
+m1 = df["ID"].str.contains(text_search)
+df_search = df[m1]
+if text_search:
+    st.write(df_search)
+
+# filter category via 
+list = df.Category.unique()
+selection = st.multiselect('Select list', list, ['Food','Hobbies','Households'])
+
+df_selection = df[df.Category.isin(selection)]
+
+
+
+# Display DataFrame
+df_editor = st.dataframe(df_selection)
+
+print("Hello")
