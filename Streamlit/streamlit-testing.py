cada71bbc9ae0048bc0e53d6ac9a123cd1186a5d
Ke Er Ang <kang0017@student.monash.edu>
2024-03-29 10:51:17 +1100

    remove redundant

--- Streamlit/streamlit-testing.py
+++ Streamlit/streamlit-testing.py
@@ -10,8 +10,6 @@ from time import time
 connection = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = 'Keer2001!',
-    database = 'new_schema'
     password = 'root',
     database = 'hobbies'
 )
@@ -44,31 +42,10 @@ def calculate_ped(group):
     return price_coef
 
 
-import statsmodels.api as sm
-import pandas as pd
-
-def calculate_ped(group):
-    X = sm.add_constant(group['Price'])
-    model = sm.OLS(group['Quantity'], X).fit()
-    price_coef = model.params['Price']
-    # mean_sellprice = group['Price'].mean()
-    # mean_quantity = group['Quantity'].mean()
-    # ped = price_coef * (mean_sellprice / mean_quantity)
-    group.loc[:, 'PED'] = price_coef # Assign the computed PED values to the 'PED' column
-    return group
-
 # Assuming df is your DataFrame
 df = df.groupby('ID').apply(calculate_ped).reset_index(drop=True)
 
 
-insert_chunks("hobbies_df.csv")
-# search bar
-text_search = st.text_input("Search by ID", value="")
-
-m1 = df["ID"].str.contains(text_search)
-df_search = df[m1]
-if text_search:
-    st.write(df_search)
 
 
 # filter category via 
