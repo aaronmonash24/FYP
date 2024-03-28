f5a0fdc43453c15c5ca016115970081c2c2396ea
aaron9980 <lywt9980@gmail.com>
2024-03-29 09:39:50 +1100

    Data insert

--- Streamlit/streamlit-testing.py
+++ Streamlit/streamlit-testing.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import mysql.connector
 import pandas as pd
+from time import time
 
 
 
 connection = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = '',
-    database = ''
+    password = 'root',
+    database = 'hobbies'
 )
 
 
@@ -18,12 +19,12 @@ print('connected')
 
 # connect to mysql
 cursor = connection.cursor()
-cursor.execute("Select * from food2")
+cursor.execute("Select * from food")
 data = cursor.fetchall()
 print(cursor.column_names)
 
 df = pd.DataFrame(data,columns = cursor.column_names)
-
+insert_chunks("hobbies_df.csv")
 # search bar
 text_search = st.text_input("Search by ID", value="")
 
@@ -44,3 +45,22 @@ df_selection = df[df.Category.isin(selection)]
 df_editor = st.dataframe(df_selection)
 
 print("Hello")
+
+
+def insert_chunks(df):
+    df_iter = pd.read_csv(df, iterator=True, chunksize=1000)
+    df = next(df_iter)
+    %time df.to_sql(name='hobby', con=engine, if_exists='append')
+
+
+    while True: 
+    t_start = time()
+
+    df = next(df_iter)
+
+    
+    df.to_sql(name='hobby', con=engine, if_exists='append')
+
+    t_end = time()
+
+    print('inserted another chunk, took %.3f second' % (t_end - t_start))
