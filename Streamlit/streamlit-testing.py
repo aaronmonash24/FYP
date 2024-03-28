61676627fb25cdfb5b39213f3ab2e6a855f9f153
Ke Er Ang <kang0017@student.monash.edu>
2024-03-29 10:35:21 +1100

    Can calculate and show PED in the table

--- Streamlit/streamlit-testing.py
+++ Streamlit/streamlit-testing.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import mysql.connector
 import pandas as pd
-
-
+import statsmodels.api as sm 
+import numpy as np
 
 connection = mysql.connector.connect(
     host = 'localhost',
@@ -23,6 +23,38 @@ data = cursor.fetchall()
 print(cursor.column_names)
 
 df = pd.DataFrame(data,columns = cursor.column_names)
+df['Price'] = df['Price'].astype('float')
+df['Quantity'] = df['Quantity'].astype('int')
+
+# calculate PED
+def calculate_ped(group):
+    X = sm.add_constant(group['Price'])
+    model = sm.OLS(group['Quantity'], X).fit()
+    price_coef = model.params['Price']
+    print(model.params['Price'])
+    # mean_sellprice = np.mean(group['Price'])
+    # mean_quantity = np.mean(group['Quantity'])
+    # ped = price_coef * (mean_sellprice / mean_quantity)
+    group.loc[:, 'PED'] = price_coef
+    return price_coef
+
+
+import statsmodels.api as sm
+import pandas as pd
+
+def calculate_ped(group):
+    X = sm.add_constant(group['Price'])
+    model = sm.OLS(group['Quantity'], X).fit()
+    price_coef = model.params['Price']
+    # mean_sellprice = group['Price'].mean()
+    # mean_quantity = group['Quantity'].mean()
+    # ped = price_coef * (mean_sellprice / mean_quantity)
+    group.loc[:, 'PED'] = price_coef # Assign the computed PED values to the 'PED' column
+    return group
+
+# Assuming df is your DataFrame
+df = df.groupby('ID').apply(calculate_ped).reset_index(drop=True)
+
 
 # search bar
 text_search = st.text_input("Search by ID", value="")
@@ -32,11 +64,14 @@ df_search = df[m1]
 if text_search:
     st.write(df_search)
 
+
 # filter category via 
 list = df.Category.unique()
 selection = st.multiselect('Select list', list, ['Food','Hobbies','Households'])
 
 df_selection = df[df.Category.isin(selection)]
+
+
 
 
 
