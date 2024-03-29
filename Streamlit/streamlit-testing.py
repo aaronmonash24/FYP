b2bd80a2c1aa35b44de6a5b442a9abc01a1ba7cc
Ke Er Ang <kang0017@student.monash.edu>
2024-03-29 11:09:26 +1100

    PED column working

--- Streamlit/streamlit-testing.py
+++ Streamlit/streamlit-testing.py
@@ -3,25 +3,18 @@ import mysql.connector
 import pandas as pd
 import statsmodels.api as sm 
 import numpy as np
-from time import time
-
-
 
 connection = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = 'root',
-    database = 'hobbies'
+    password = '',
+    database = 'new_schema'
 )
-
-
 st.title('my FIT3164 streamlit hehe')
-
 print('connected')
-
 # connect to mysql
 cursor = connection.cursor()
-cursor.execute("Select * from food")
+cursor.execute("Select * from food2")
 data = cursor.fetchall()
 print(cursor.column_names)
 
@@ -42,16 +35,34 @@ def calculate_ped(group):
     return price_coef
 
 
+import statsmodels.api as sm
+import pandas as pd
+
+def calculate_ped(group):
+    X = sm.add_constant(group['Price'])
+    model = sm.OLS(group['Quantity'], X).fit()
+    price_coef = model.params['Price']
+    # mean_sellprice = group['Price'].mean()
+    # mean_quantity = group['Quantity'].mean()
+    # ped = price_coef * (mean_sellprice / mean_quantity)
+    group.loc[:, 'PED'] = price_coef # Assign the computed PED values to the 'PED' column
+    return group
+
 # Assuming df is your DataFrame
 df = df.groupby('ID').apply(calculate_ped).reset_index(drop=True)
 
 
+# search bar
+text_search = st.text_input("Search by ID", value="")
+m1 = df["ID"].str.contains(text_search)
+df_search = df[m1]
+if text_search:
+    st.write(df_search)
 
 
 # filter category via 
 list = df.Category.unique()
 selection = st.multiselect('Select list', list, ['Food','Hobbies','Households'])
-
 df_selection = df[df.Category.isin(selection)]
 
 
@@ -60,5 +71,3 @@ df_selection = df[df.Category.isin(selection)]
 
 # Display DataFrame
 df_editor = st.dataframe(df_selection)
-
-print("Hello")
