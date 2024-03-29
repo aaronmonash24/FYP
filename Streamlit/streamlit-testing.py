419de3f68b98e69ec3aef7d4696db02fd078f989
Ke Er Ang <kang0017@student.monash.edu>
2024-03-29 11:15:17 +1100

    PED working

--- Streamlit/streamlit-testing.py
+++ Streamlit/streamlit-testing.py
@@ -4,40 +4,25 @@ import pandas as pd
 import statsmodels.api as sm 
 import numpy as np
 
+# connect mysql
 connection = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
     password = '',
-    database = 'new_schema'
+    database = ''
 )
-st.title('my FIT3164 streamlit hehe')
-print('connected')
-# connect to mysql
+
 cursor = connection.cursor()
 cursor.execute("Select * from food2")
 data = cursor.fetchall()
-print(cursor.column_names)
 
+# create dataframe
+st.title('my FIT3164 streamlit hehe')
 df = pd.DataFrame(data,columns = cursor.column_names)
 df['Price'] = df['Price'].astype('float')
 df['Quantity'] = df['Quantity'].astype('int')
 
 # calculate PED
-def calculate_ped(group):
-    X = sm.add_constant(group['Price'])
-    model = sm.OLS(group['Quantity'], X).fit()
-    price_coef = model.params['Price']
-    print(model.params['Price'])
-    # mean_sellprice = np.mean(group['Price'])
-    # mean_quantity = np.mean(group['Quantity'])
-    # ped = price_coef * (mean_sellprice / mean_quantity)
-    group.loc[:, 'PED'] = price_coef
-    return price_coef
-
-
-import statsmodels.api as sm
-import pandas as pd
-
 def calculate_ped(group):
     X = sm.add_constant(group['Price'])
     model = sm.OLS(group['Quantity'], X).fit()
@@ -48,7 +33,6 @@ def calculate_ped(group):
     group.loc[:, 'PED'] = price_coef # Assign the computed PED values to the 'PED' column
     return group
 
-# Assuming df is your DataFrame
 df = df.groupby('ID').apply(calculate_ped).reset_index(drop=True)
 
 
@@ -64,8 +48,6 @@ if text_search:
 list = df.Category.unique()
 selection = st.multiselect('Select list', list, ['Food','Hobbies','Households'])
 df_selection = df[df.Category.isin(selection)]
-
-
 
 
 
